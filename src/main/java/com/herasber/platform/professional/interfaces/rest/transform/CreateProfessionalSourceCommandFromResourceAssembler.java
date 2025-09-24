b6fc085f67e0b14d805f23e93df8e51655b64a6f
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-23 21:27:53 -0500

    feat(professional): add assembler to map CreateProfessionalSourceResource → CreateProfessionalSourceCommand

---
+++ src/main/java/com/herasber/platform/professional/interfaces/rest/transform/CreateProfessionalSourceCommandFromResourceAssembler.java
@@ -0,0 +1,13 @@
+package com.herasber.platform.professional.interfaces.rest.transform;
+
+import com.herasber.platform.professional.domain.model.commands.CreateProfessionalSourceCommand;
+import com.herasber.platform.professional.interfaces.rest.resources.CreateProfessionalSourceResource;
+
+public class CreateProfessionalSourceCommandFromResourceAssembler {
+    public static CreateProfessionalSourceCommand toCommandFromResource(CreateProfessionalSourceResource r){
+        return new CreateProfessionalSourceCommand(
+                r.fullName(), r.phone(), r.servicesDescription(), r.photoUrl(),
+                r.cityName(), r.districtName(), r.mapsUrl()
+        );
+    }
+}
