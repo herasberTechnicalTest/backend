2a3d9b336757b0c562446f207baea8d120c8f633
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-24 10:58:16 -0500

    feat(professional-service): update query service

--- src/main/java/com/herasber/platform/professional/domain/services/ProfessionalSourceQueryService.java
+++ src/main/java/com/herasber/platform/professional/domain/services/ProfessionalSourceQueryService.java
@@ -8,5 +8,4 @@ public interface ProfessionalSourceQueryService {
     List<com.herasber.platform.professional.domain.model.aggregates.ProfessionalSource> handleGetAllByDistrict(String districtName);
     Optional<com.herasber.platform.professional.domain.model.aggregates.ProfessionalSource> handleGetById(Long id);
     List<com.herasber.platform.professional.domain.model.aggregates.ProfessionalSource> handleGetAll();
-    // 👈 nuevo
 }
