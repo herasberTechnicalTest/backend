cea7ec3ed0e108e93ee2cf7e43135429fd294eb8
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-24 10:55:38 -0500

    feat(professional-service): add handleGetAll() to query service

--- src/main/java/com/herasber/platform/professional/domain/services/ProfessionalSourceQueryService.java
+++ src/main/java/com/herasber/platform/professional/domain/services/ProfessionalSourceQueryService.java
@@ -7,4 +7,6 @@ public interface ProfessionalSourceQueryService {
     List<com.herasber.platform.professional.domain.model.aggregates.ProfessionalSource> handleGetAllByCity(String cityName);
     List<com.herasber.platform.professional.domain.model.aggregates.ProfessionalSource> handleGetAllByDistrict(String districtName);
     Optional<com.herasber.platform.professional.domain.model.aggregates.ProfessionalSource> handleGetById(Long id);
+    List<com.herasber.platform.professional.domain.model.aggregates.ProfessionalSource> handleGetAll();
+    // 👈 nuevo
 }
