4dd70f9b327f6ed017f147767c739e58c6d5fec7
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-23 19:08:45 -0500

    chore(init): initialize Spring Boot project

---
+++ src/test/java/com/herasber/TechnicalTest/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herasber.TechnicalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
