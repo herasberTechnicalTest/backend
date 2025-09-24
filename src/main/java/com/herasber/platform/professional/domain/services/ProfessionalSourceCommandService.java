81d528c5a8068af5664537d07570495703df249d
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-24 10:55:24 -0500

    feat(professional-service): add update and delete operations to command service API

--- src/main/java/com/herasber/platform/professional/domain/services/ProfessionalSourceCommandService.java
+++ src/main/java/com/herasber/platform/professional/domain/services/ProfessionalSourceCommandService.java
@@ -1,10 +1,13 @@
 package com.herasber.platform.professional.domain.services;
 
 import com.herasber.platform.professional.domain.model.commands.CreateProfessionalSourceCommand;
+import com.herasber.platform.professional.domain.model.commands.UpdateProfessionalSourceCommand;
 
 import java.util.Optional;
 
+
 public interface ProfessionalSourceCommandService {
     com.herasber.platform.professional.domain.model.aggregates.ProfessionalSource handle(CreateProfessionalSourceCommand command);
-
+    Optional<com.herasber.platform.professional.domain.model.aggregates.ProfessionalSource> update(Long id, UpdateProfessionalSourceCommand command);
+    boolean delete(Long id);
 }
