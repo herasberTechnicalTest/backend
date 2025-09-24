4982ce885324130781ec45154f983e187ae9c81c
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-23 21:28:03 -0500

    feat(professional): add input DTO CreateProfessionalSourceResource with validations

---
+++ src/main/java/com/herasber/platform/professional/interfaces/rest/resources/CreateProfessionalSourceResource.java
@@ -0,0 +1,23 @@
+package com.herasber.platform.professional.interfaces.rest.resources;
+
+public record CreateProfessionalSourceResource(
+        String fullName,
+        String phone,
+        String servicesDescription,
+        String photoUrl,
+        String cityName,
+        String districtName,
+        String mapsUrl
+) {
+    public CreateProfessionalSourceResource {
+        if (fullName == null || fullName.isBlank())
+            throw new IllegalArgumentException("fullName cannot be null or empty");
+        if (phone == null || phone.isBlank()) throw new IllegalArgumentException("phone cannot be null or empty");
+        if (servicesDescription == null || servicesDescription.isBlank())
+            throw new IllegalArgumentException("servicesDescription cannot be null or empty");
+        if (cityName == null || cityName.isBlank())
+            throw new IllegalArgumentException("cityName cannot be null or empty");
+        if (districtName == null || districtName.isBlank())
+            throw new IllegalArgumentException("districtName cannot be null or empty");
+    }
+}
