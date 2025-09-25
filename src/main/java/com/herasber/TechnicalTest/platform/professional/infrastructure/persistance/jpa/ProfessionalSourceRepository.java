645f7c5c3884af7b3b09afa8757470a58874ec99
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-24 22:37:11 -0500

    feat(repo): add email lookup and case-insensitive existence checks

--- src/main/java/com/herasber/TechnicalTest/platform/professional/infrastructure/persistance/jpa/ProfessionalSourceRepository.java
+++ src/main/java/com/herasber/TechnicalTest/platform/professional/infrastructure/persistance/jpa/ProfessionalSourceRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProfessionalSourceRepository extends JpaRepository<ProfessionalSource, Long> {
     List<ProfessionalSource> findAll();
     List<ProfessionalSource> findAllByLocation_CityName(String cityName);
     List<ProfessionalSource> findAllByLocation_DistrictName(String districtName);
+    Optional<ProfessionalSource> findByEmail(String email);
+    boolean existsByEmailNorm(String emailNorm);
+    Optional<ProfessionalSource> findByEmailNorm(String emailNorm);
 }
