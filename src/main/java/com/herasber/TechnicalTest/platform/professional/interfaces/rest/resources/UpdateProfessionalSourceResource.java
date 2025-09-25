fd6a3a7b7a3d46c551c79778b07e0a23611f2c69
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-24 22:38:13 -0500

    feat(api): add update resource with optional password, gallery, pricing, and location

--- src/main/java/com/herasber/TechnicalTest/platform/professional/interfaces/rest/resources/UpdateProfessionalSourceResource.java
+++ src/main/java/com/herasber/TechnicalTest/platform/professional/interfaces/rest/resources/UpdateProfessionalSourceResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 public record UpdateProfessionalSourceResource(
         Long id,
         String fullName,
+        String email, String password,
         String phone,
         String servicesDescription,
         String photoUrl,
